e271f723bd3aab3cec1f3833252094fc943852de
blu2ego <blu2ego@gmail.com>
2020-07-13 11:42:26 +0900

    change assign operator from = to <-

--- crawling/00_environment.R
+++ crawling/00_environment.R
@@ -2,13 +2,13 @@ library("rvest")
 
 source("01_keys.R")
 
-xml_child2df = function(x){
-  x_child = html_children(x)
+xml_child2df <- function(x){
+  x_child <- html_children(x)
   
-  x_df = as.data.frame(matrix(html_text(x_child), 
+  x_df <- as.data.frame(matrix(html_text(x_child), 
                               byrow = TRUE, 
                               nrow = 1))
-  colnames(x_df) = html_name(x_child)
+  colnames(x_df) <- html_name(x_child)
   return(x_df)
 }
 
