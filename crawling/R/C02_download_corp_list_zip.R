f31a453cf02d21e4ec16eaff99911ddb0c966463
blu2ego <blu2ego@gmail.com>
2020-07-14 15:11:11 +0900

    add comments

--- crawling/R/C02_download_corp_list_zip.R
+++ crawling/R/C02_download_corp_list_zip.R
@@ -8,3 +8,9 @@ download.file(url = url_code,
               destfile = file_name,
               mode = "wb", 
               quiet = TRUE)
+
+# On Windows, if mode is not supplied (missing()) and url ends in one of .gz, .bz2, .xz, .tgz, .zip, .rda, .rds or 
+# .RData, mode = "wb" is set such that a binary transfer is done to help unwary users.
+
+# Code written to download binary files must use mode = "wb" (or "ab"), 
+# but the problems incurred by a text transfer will only be seen on Windows.
