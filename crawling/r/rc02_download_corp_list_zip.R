14fb77ee0cb79cc44abc968d578d59fcd52fca00
blu2ego <blu2ego@gmail.com>
2020-07-14 18:13:54 +0900

    all restruturing

--- crawling/r/rc02_download_corp_list_zip.R
+++ crawling/r/rc02_download_corp_list_zip.R
@@ -1,8 +1,8 @@
-source("~/projects/wrangling_accounting_related_data/crawling/R/C01_environment.R", encoding = "UTF-8")
+source("~/projects/wrangling_accounting_related_data/crawling/R/rc01_environment.R", encoding = "UTF-8")
 
 file_name <- "codebook.zip"
 
-url_code <- paste0("https://opendart.fss.or.kr/api/corpCode.xml?&crtfc_key=", key_dart) 
+url_code <- paste0("https://opendart.fss.or.kr/api/corpCode.xml?&crtfc_key=", key_dart)
 
 # Comments: When use download.file() function on Windows, should be careful as below:
 # On Windows, if mode is not supplied (missing()) and url ends in one of .gz, .bz2, .xz, .tgz, .zip, .rda, .rds or 
