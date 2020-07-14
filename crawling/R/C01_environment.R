0fb8542670c599e13673b1f1a7c16ab83e4dd94f
blu2ego <blu2ego@gmail.com>
2020-07-14 15:09:14 +0900

    fix file name

--- crawling/R/C01_environment.R
+++ crawling/R/C01_environment.R
@@ -1,7 +1,9 @@
 # Setting Global Environments
 
+# importing dart's key
 key_dart <- readLines("~/projects/wrangling_accounting_related_data/crawling/sources/key_dart.txt")
 
+# creating xml_child2df function to import and process xml
 library(rvest)
 xml_child2df <- function(x){
   x_child <- html_children(x)
