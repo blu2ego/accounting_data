58775f983e32fec5c97b3d905dc2a9fbf8c4cd8d
blu2ego <blu2ego@gmail.com>
2020-07-14 15:13:41 +0900

    fix comments

--- crawling/R/C02_download_corp_list_zip.R
+++ crawling/R/C02_download_corp_list_zip.R
@@ -4,13 +4,14 @@ file_name <- "codebook.zip"
 
 url_code <- paste0("https://opendart.fss.or.kr/api/corpCode.xml?&crtfc_key=", key_dart) 
 
+# Comments: When use download.file() function on Windows, should be careful as below:
+# On Windows, if mode is not supplied (missing()) and url ends in one of .gz, .bz2, .xz, .tgz, .zip, .rda, .rds or 
+# .RData, mode = "wb" is set such that a binary transfer is done to help unwary users.
+# Code written to download binary files must use mode = "wb" (or "ab"), 
+# but the problems incurred by a text transfer will only be seen on Windows.
+
 download.file(url = url_code, 
               destfile = file_name,
               mode = "wb", 
               quiet = TRUE)
 
-# On Windows, if mode is not supplied (missing()) and url ends in one of .gz, .bz2, .xz, .tgz, .zip, .rda, .rds or 
-# .RData, mode = "wb" is set such that a binary transfer is done to help unwary users.
-
-# Code written to download binary files must use mode = "wb" (or "ab"), 
-# but the problems incurred by a text transfer will only be seen on Windows.
