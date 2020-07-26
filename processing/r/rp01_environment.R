10229537d92c1b8c49038722f45eff5ff033286b
encaion <encaionkim@gmail.com>
2020-07-26 23:32:36 +0900

    R - processing: modify environment file for rp02 and rp03 files.

--- processing/r/rp01_environment.R
+++ processing/r/rp01_environment.R
@@ -1,3 +1,19 @@
+library(rvest)
+library(stringi)
+library(jsonlite)
+library(tabulizer)
+
 base_date <- Sys.Date()
-biz_report_doc <- paste0("results/biz_report/doc/", base_date, "/")
-audit_report_doc <- paste0("results/audit_report/doc/", base_date, "/")
+
+# A001
+biz_report_doc             <- paste0("results/biz_report/doc/",  base_date, "/")
+biz_report_list_csv_Dir    <- paste0("results/biz_report/csv/",  base_date, "/")
+biz_report_parsed_rds_Dir  <- paste0("results/biz_report/rds/",  base_date, "/")
+biz_report_parsed_json_Dir <- paste0("results/biz_report/json/", base_date, "/")
+
+
+# F001
+audit_report_doc             <- paste0("results/audit_report/doc/",  base_date, "/")
+audit_report_list_csv_Dir    <- paste0("results/audit_report/csv/",  base_date, "/")
+audit_report_parsed_rds_Dir  <- paste0("results/audit_report/rds/",  base_date, "/")
+audit_report_parsed_json_Dir <- paste0("results/audit_report/json/", base_date, "/")
