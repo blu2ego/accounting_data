4efb6af6fa52670eb036a30f64d496af1906e926
encaion <encaionkim@gmail.com>
2020-07-26 15:35:40 +0900

    delete comments

--- crawling/python/pc01_environment.py
+++ crawling/python/pc01_environment.py
@@ -1,23 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
+import os
+import time
 
 key_dart_file = open("C:/Users/Encaion/Documents/41_outsource/crawling_DART_package/crawling/sources/key_dart.txt", 'r')
 key_dart = key_dart_file.readline()
 key_dart_file.close()
-
-
-# In[10]:
-
-
-import os
-import time
-
-
-# In[27]:
-
 
 # mainDir = "~/projects/wrangling_accounting_related_data/"
 mainDir = "C:/Users/Encaion/Documents/41_outsource/crawling_DART_package"
@@ -29,10 +15,6 @@ corps_code_zip_Dir = "results/corps_code/zip/"
 corps_code_unzip_Dir = "results/corps_code/xml/"
 corps_code_parsed_csv_Dir = "results/corps_code/csv/"
 
-
-# In[26]:
-
-
 if not os.path.exists(mainDir + corps_code_zip_Dir):
     os.makedirs(mainDir + corps_code_zip_Dir)
 if not os.path.exists(mainDir + corps_code_unzip_Dir):
@@ -40,35 +22,18 @@ if not os.path.exists(mainDir + corps_code_unzip_Dir):
 if not os.path.exists(mainDir + corps_code_parsed_csv_Dir):
     os.makedirs(mainDir + corps_code_parsed_csv_Dir)
 
-
-# In[29]:
-
-
 audit_report_list_xml_Dir = "results/audit_report/audit_report_list/" + base_date + "/"
 audit_report_zip = "results/audit_report/zip/" + base_date + "/"
-
-
-# In[ ]:
-
 
 if not os.path.exists(mainDir + audit_report_list_xml_Dir):
     os.makedirs(mainDir + audit_report_list_xml_Dir)
 if not os.path.exists(mainDir + audit_report_zip):
     os.makedirs(mainDir + audit_report_zip)
 
-
-# In[ ]:
-
-
 biz_report_list_xml_Dir = "results/biz_report/biz_report_list/" + base_date + "/"
 biz_report_zip = "results/biz_report/zip/" + base_date + "/")
-
-
-# In[ ]:
-
 
 if not os.path.exists(mainDir + biz_report_list_xml_Dir):
     os.makedirs(mainDir + biz_report_list_xml_Dir)
 if not os.path.exists(mainDir + biz_report_zip):
     os.makedirs(mainDir + biz_report_zip)
-
