aeb00b0581635f81c91f435643c6cdbc052d75f5
encaion <encaionkim@gmail.com>
2020-07-26 19:31:21 +0900

    R - crawling: add some new objects for maintenance.

--- crawling/r/dart/rc05_downloader_F001_list_xml.R
+++ crawling/r/dart/rc05_downloader_F001_list_xml.R
@@ -1,5 +1,8 @@
 source("~/projects/wrangling_accounting_related_data/crawling/r/dart/rc01_environment.R", encoding = "UTF-8")
 
+# initialization
+setwd("./")
+
 corps_code_f001 <- read.csv(paste0(mainDir, corps_code_parsed_csv_Dir, "corps_code_parsed_", base_date, ".csv"), 
                             header = T)
 
@@ -8,11 +11,14 @@ date_end_f001 <- gsub(pattern = "[^0-9]", replacement = "", x = base_date)
 last_reprt_at_f001 <- "Y"
 pblntf_ty_f001 <- "F"
 pblntf_detail_ty_f001 <- "F001"
+page_count_f001 <- 100
+
 
 start_f001 <- 1
+end_f001 <- nrow(corps_code_f001)
 time_delay_f001 <- 5
 
-for(j in start_f001:nrow(corps_code_f001)){
+for(j in start_f001:end_f001){
   print(j)
   corp_code_f001 <- sprintf(fmt = "%08d", corps_code_f001[j, "corp_code"])
   corp_name_f001 <- corps_code_f001[j, "corp_name"]
@@ -25,10 +31,11 @@ for(j in start_f001:nrow(corps_code_f001)){
                              "&last_reprt_at", last_reprt_at_f001,
                              "&pblntf_ty=", pblntf_ty_f001,
                              "&pblntf_detail_ty=", pblntf_detail_ty_f001,
-                             "&page_count=", 100)
+                             "&page_count=", page_count_f001)
   
   report_f001 <- read_html(request_url_f001, encoding = "UTF-8")
-  write_xml(report_f001, paste0(mainDir, audit_report_list_xml_Dir, "f001_", corp_code_f001, ".xml"), 
+  write_xml(report_f001, 
+            paste0(mainDir, audit_report_list_xml_Dir, "f001_", corp_code_f001, ".xml"), 
             encoding = "UTF-8")
   
   Sys.sleep(time_delay_f001 + runif(1) * 2)
