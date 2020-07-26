5862a61b242e88f2b0be6a8e5fd001ba1aafce59
encaion <encaionkim@gmail.com>
2020-07-26 20:17:26 +0900

    R - crawling: update doc list parser

--- crawling/r/dart/rc07_downloaded_F001_list_xml_parser.R
+++ crawling/r/dart/rc07_downloaded_F001_list_xml_parser.R
@@ -1,7 +1,7 @@
 source("~/projects/wrangling_accounting_related_data/crawling/r/dart/rc01_environment.R", encoding = "UTF-8")
 
-listt <- list.files(path = biz_report_list_xml_Dir,
-                    pattern = "a001",
+listt <- list.files(path = audit_report_list_xml_Dir,
+                    pattern = "f001",
                     full.names = TRUE)
 
 for(n in 1:length(listt)){
@@ -11,12 +11,10 @@ for(n in 1:length(listt)){
     lapply("xml_child2df") %>% 
     do.call(what = "rbind") -> df_codelist
   if(!is.null(df_codelist[1, "rcept_no"])){
-    df_codelist <- df_codelist[grep(pattern = "사업보고서", df_codelist$report_nm), ]
-    
     if(nrow(df_codelist) >= 1){
       print(n)
       write.csv(df_codelist, 
-                paste0(biz_report_list_csv_Dir, "/doc_a001_list_parsed_", df_codelist[1, "corp_code"], ".csv"),
+                paste0(audit_report_list_csv_Dir, "/doc_f001_list_parsed_", df_codelist[1, "corp_code"], ".csv"),
                 row.names = FALSE) 
     }
   }
