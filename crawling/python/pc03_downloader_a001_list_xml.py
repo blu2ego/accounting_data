48ad8880e0eae1b047e5dec3a935e5fb3da8819f
encaion <encaionkim@gmail.com>
2020-08-01 18:26:29 +0900

    Py - crawling: Update a001 document lists and sample files.

--- crawling/python/pc03_downloader_a001_list_xml.py
+++ crawling/python/pc03_downloader_a001_list_xml.py
@@ -5,39 +5,47 @@ os.chdir(mainDir + corps_code_parsed_csv_Dir)
 
 # loading required libraries
 import pandas as pd
+from numpy import arange
+import requests
+import random
 
 corps_code_a001 = pd.read_csv("corps_code_parsed_" + base_date + ".csv")
 
+os.chdir("../../..")
+
 date_begin_a001 = "19800101"
-date_end_a001 = base_date
+date_end_a001 = base_date.replace("-", "")
 last_reprt_at_a001 = "Y"
 pblntf_ty_a001 = "A"
 pblntf_detail_ty_a001 = "A001"
 page_count_a001 = 100
 
-start_a001 = 1
-end_a001 = nrow(corps_code_a001)
-time_delay_a001 = 5
+# start_a001 = 0
+# end_a001 = len(corps_code_a001) - 1
+start_a001 = 30000
+end_a001 = 30010
+time_delay_a001 = 1
 
-for(i in start_a001:end_a001){
+for i in arange(start_a001, end_a001):
   print(i)
-  corp_code_a001 = sprintf(fmt = "%08d", corps_code_a001[i, "corp_code"])
-  corp_name_a001 = corps_code_a001[i, "corp_name"]
+  corp_code_a001 = "{:08}".format(corps_code_a001.loc[i, "corp_code"])
+  corp_name_a001 = corps_code_a001.loc[i, "corp_name"]
   
-  request_url_a001 = paste0("https://opendart.fss.or.kr/api/list.xml?",
-                             "&crtfc_key=", key_dart,
-                             "&corp_code=", corp_code_a001,
-                             "&bgn_de=", date_begin_a001,
-                             "&end_de=", date_end_a001,
-                             "&last_reprt_at", last_reprt_at_a001,
-                             "&pblntf_ty=", pblntf_ty_a001,
-                             "&pblntf_detail_ty=", pblntf_detail_ty_a001,
-                             "&page_count=", page_count_a001)
+  request_url_a001 = "".join(["https://opendart.fss.or.kr/api/list.xml?", 
+                              "&crtfc_key=", key_dart,
+                              "&corp_code=", corp_code_a001,
+                              "&bgn_de=", date_begin_a001,
+                              "&end_de=", date_end_a001,
+                              "&last_reprt_at", last_reprt_at_a001,
+                              "&pblntf_ty=", pblntf_ty_a001,
+                              "&pblntf_detail_ty=", pblntf_detail_ty_a001,
+                              "&page_count=", str(page_count_a001)])
   
-  report_a001 = read_html(request_url_a001, encoding = "UTF-8")
-  write_xml(report_a001, 
-            paste0(mainDir, biz_report_list_xml_Dir, "a001_", corp_code_a001, ".xml"), 
-            encoding = "UTF-8")
+  report_a001 = requests.get(request_url_a001)
   
-  Sys.sleep(time_delay_a001 + runif(1) * 2)
-}
+  xml_file_name = mainDir + biz_report_list_xml_Dir + "a001_" + corp_code_a001 + ".xml"
+  
+  with open(xml_file_name, "wb") as file:
+      file.write(report_a001.content)
+  
+  time.sleep(time_delay_a001 + random.uniform(0, 1) * 2)
