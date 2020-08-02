d5efd132cbcc4bcd50dad455873cfecf2bf66f91
encaion <encaionkim@gmail.com>
2020-08-02 20:36:29 +0900

    Synchronizing R & Py scripts - corps code zip file downloader(crawling)

--- crawling/dart/python/pc02_downloader_corps_code_zip.py
+++ crawling/dart/python/pc02_downloader_corps_code_zip.py
@@ -6,7 +6,7 @@ os.chdir(mainDir + corps_code_zip_Dir)
 # loading required libraries
 import urllib.request
 
-corps_code_raw = "corps_code_" + base_date + ".zip"
-corps_code_request_url = "https://opendart.fss.or.kr/api/corpCode.xml?&crtfc_key=" + key_dart
+base_date_corps_code_zip = "corps_code_" + base_date + ".zip"
+request_url_corps_code = "https://opendart.fss.or.kr/api/corpCode.xml?&crtfc_key=" + key_dart
 
-urllib.request.urlretrieve(corps_code_request_url, corps_code_raw)
+urllib.request.urlretrieve(request_url_corps_code, base_date_corps_code_zip)
