7ef1fef5d5714d1970f27db0512fac9ba6efb248
encaion <encaionkim@gmail.com>
2020-08-02 21:39:40 +0900

    Synchronizing R & Py scripts - a001 list xml parsing file(processing)

--- processing/dart/python/pp04_downloaded_a001_list_xml_parser.py
+++ processing/dart/python/pp04_downloaded_a001_list_xml_parser.py
@@ -1,35 +1,34 @@
 exec(open("C:/Users/Encaion/Documents/41_outsource/crawling_DART_package/crawling/python/pc01_environment.py").read())  
 
 # set working directory
-os.chdir(mainDir + biz_report_list_xml_Dir)
+os.chdir(main_dir + biz_report_list_xml_dir)
 
 # loading required libraries
 from bs4 import BeautifulSoup
 import pandas as pd
 
-biz_reports = os.listdir()
+list_a001s_xml = os.listdir()
 
-for n in range(len(biz_reports)):
-  biz_report = open(file = biz_reports[n], mode = "r", encoding = "UTF-8")
-  biz_report = BeautifulSoup(biz_report).find_all("list")
+for n in range(len(list_a001s_xml)):
+  list_a001 = open(file = list_a001s_xml[n], mode = "r", encoding = "UTF-8")
+  list_a001 = BeautifulSoup(list_a001).find_all("list")
   
-  if len(biz_report) > 0:
-    df_biz_report = pd.DataFrame()
-    for biz_report_sub in biz_report:
-      # biz_report_sub = biz_report[0]
-      df_biz_report_sub = pd.DataFrame([[biz_report_sub.select("corp_code" )[0].text,
-                                         biz_report_sub.select("corp_name" )[0].text,
-                                         biz_report_sub.select("stock_code")[0].text,
-                                         biz_report_sub.select("corp_cls"  )[0].text,
-                                         biz_report_sub.select("report_nm" )[0].text,
-                                         biz_report_sub.select("rcept_no"  )[0].text,
-                                         biz_report_sub.select("flr_nm"    )[0].text,
-                                         biz_report_sub.select("rcept_dt"  )[0].text,
-                                         biz_report_sub.select("rm"        )[0].text]])
-      df_biz_report = pd.concat([df_biz_report, df_biz_report_sub])
+  if len(list_a001) > 0:
+    df_list_a001 = pd.DataFrame()
+    for list_a001_sub in list_a001:
+      df_list_a001_sub = pd.DataFrame([[list_a001_sub.select("corp_code" )[0].text,
+                                        list_a001_sub.select("corp_name" )[0].text,
+                                        list_a001_sub.select("stock_code")[0].text,
+                                        list_a001_sub.select("corp_cls"  )[0].text,
+                                        list_a001_sub.select("report_nm" )[0].text,
+                                        list_a001_sub.select("rcept_no"  )[0].text,
+                                        list_a001_sub.select("flr_nm"    )[0].text,
+                                        list_a001_sub.select("rcept_dt"  )[0].text,
+                                        list_a001_sub.select("rm"        )[0].text]])
+      df_list_a001 = pd.concat([df_list_a001, df_list_a001_sub])
    
-    df_biz_report.columns = ["corp_code", "corp_name", "stock_code", "corp_cls", "report_nm", "rcept_no", "flr_nm", "rcept_dt", "rm"]
-    df_biz_report = df_biz_report.loc[df_biz_report["report_nm"].str.contains("사업보고서"), ]
+    df_list_a001.columns = ["corp_code", "corp_name", "stock_code", "corp_cls", "report_nm", "rcept_no", "flr_nm", "rcept_dt", "rm"]
+    df_list_a001 = df_list_a001.loc[df_list_a001["report_nm"].str.contains("사업보고서"), ]
     
-    if len(df_biz_report) > 0:
-      df_biz_report.to_csv("../../csv/" + base_date + "/doc_a001_list_parsed_" + df_biz_report.iloc[0, 0] + ".csv", index = False)
+    if len(df_list_a001) > 0:
+      df_list_a001.to_csv("../../csv/" + base_date + "/doc_a001_list_parsed_" + df_list_a001.iloc[0, 0] + ".csv", index = False)
