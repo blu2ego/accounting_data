b785d4fd1c017c7029380a55f4b403be4e8820c2
blu2ego <blu2ego@gmail.com>
2020-08-07 20:23:29 +0900

    make code clean

--- processing/dart/r/samples/rps01_external_audit.R
+++ processing/dart/r/samples/rps01_external_audit.R
@@ -40,29 +40,29 @@ prf_time_yearend <- c("전산감사 등 전문가" = 4287,
 auditors_time_yearend <- c(quality_ctrl_time_yearend, cpa_time_yearend, prf_time_yearend)
 
 # 외부 감사 실시 내용(집합)
-external_audit_contents <- list("삼성전자_00126380" = 
-                                  list("fiscal_year" = 
-                                         list("year_end" = "1231", 
-                                              "corp_cls" = "Y", 
-                                              "corp_name" = "삼성전자",
-                                              "corp_code" = "00126380", 
-                                              "stock_code" = "005930", 
-                                              "report_name" = "감사보고서",  
-                                              "rcept_no" = "20030807000168", 
-                                              "flr_name" = "삼일회계법인", 
-                                              "rcept_dt" = "20030807", 
-                                              "rm" = "Y",
-                                              "감사참여자 구분별 인원수 및 감사시간" = 
-                                                list("투입 인원수" = auditors, 
-                                                     "분/반기검토(시간)" = auditors_time_periodic, 
-                                                     "감사(시간)" = auditors_time_yearend))
-                                       )
-                                )
+external_audit <- list("corp_code" = 
+                         list("fiscal_year" = 
+                                list("year_end" = "1231", 
+                                     "corp_cls" = "Y", 
+                                     "corp_name" = "삼성전자",
+                                     "corp_code" = "00126380", 
+                                     "stock_code" = "005930", 
+                                     "report_name" = "감사보고서",  
+                                     "rcept_no" = "20030807000168", 
+                                     "flr_name" = "삼일회계법인", 
+                                     "rcept_dt" = "20030807", 
+                                     "rm" = "Y",
+                                     "감사참여자 구분별 인원수 및 감사시간" = 
+                                       list("투입 인원수" = auditors, 
+                                            "분/반기검토(시간)" = auditors_time_periodic, 
+                                            "감사(시간)" = auditors_time_yearend))
+                              )
+                       )
 
 
 # 외부 감사 실시 내용(집합)을 josn으로 변형
-external_audit_contents <- jsonlite::toJSON(external_audit_contents, pretty = TRUE, auto_unbox = TRUE)
+external_audit <- jsonlite::toJSON(external_audit_contents, pretty = TRUE, auto_unbox = TRUE)
 
 # json 파일로 writing 하려면,
-write(external_audit_contents, "~/projects/ward/results/dart/for_accounting/samples/external_audit_contents.json") 
+write(external_audit, "~/projects/ward/results/dart/for_accounting/samples/external_audit.json") 
 
