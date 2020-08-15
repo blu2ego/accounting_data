a994723c6d38fbfcc2b550040afaba3e77b6424c
encaion <encaionkim@gmail.com>
2020-08-16 00:57:22 +0900

    Py - crawling: update f001 pdf downloader

--- crawling/dart/python/pc08_downloader_f-f001_pdf.py
+++ crawling/dart/python/pc08_downloader_f-f001_pdf.py
@@ -1,6 +1,9 @@
 # set working directory
 os.chdir(main_dir + audit_report_list_csv_dir)
 
+# loading required libraries
+from bs4 import BeautifulSoup
+
 # importing list of F001 files
 list_f001s_csv = os.listdir()
 
@@ -24,7 +27,7 @@ for n_f001s in arange(start_f001, end_f001):
   if len(list_f001) > 0:
     corp_code_f001 = "{:08}".format(list_f001.loc[0, "corp_code"]) 
     
-    # path_dir = paste0(main_dir, audit_report_pdf_from_aud, "corp_code_", corp_code)
+    path_dir = main_dir + audit_report_pdf_from_aud + "corp_code_" + corp_code_f001
     # dir.create(path = path_dir, showWarnings = FALSE)
     
     for n_f001 in 0:len(list_f001):
@@ -32,18 +35,16 @@ for n_f001s in arange(start_f001, end_f001):
       rcept_no_f001 = list_f001.loc[n_f001, "rcept_no"].astype("str")
       request_url_f001 = request_url_base_f001 + rcept_no_f001
       
-      # f001 = read_html(request_url_f001, encoding = "UTF-8")
-      # f001 %>% 
-      #   html_nodes(xpath = "//*/a[@href='#download']") %>% 
-      #   html_attr(name = "onclick") %>%
-      #   stri_extract(regex = "\\'[0-9]{6,8}\\'") %>% 
-      #   gsub(pattern = "[^0-9]", replacement = "") -> dcm_no
-      # 
-      # url_f001 = paste0(request_url_base_pdf, "rcp_no=", rcept_no_f001, "&dcm_no=", dcm_no)
-      # 
-      # download.file(url = url_f001, 
-      #               destfile = paste0(path_dir, "/f001_", corp_code, "_", rcept_no_f001, "_", dcm_no, ".pdf"),
-      #               quiet = TRUE, 
-      #               mode = "wb")
+      f001 = requests.get(request_url_f001)
+      f001 = BeautifulSoup(f001.content, from_encoding = "UTF-8")
+      
+      dcm_no = f001.select("a[href='#download']")[0].attrs["onclick"].split(sep = ", \'")[1][:7]
+      
+      url_f001 = request_url_base_pdf + "rcp_no=" + rcept_no_f001 + "&dcm_no=" + dcm_no
+      
+      path_pdf = path_dir + "/f001_" + corp_code_f001 + "_" + rcept_no_f001 + "_" + dcm_no + ".pdf"
+      
+      # need download codes
+      
       
       time.sleep(time_delay_f001 + random.uniform(0, 1) * 2)
