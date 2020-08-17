e58604c35dc548824258997b7fe40016bda5ea4d
blu2ego <blu2ego@gmail.com>
2020-08-18 06:12:47 +0900

    continuing change file path

--- crawling/dart/r/cdr01_environments.R
+++ crawling/dart/r/cdr01_environments.R
@@ -11,6 +11,7 @@ key_dart <- readLines("~/projects/ward/crawling/sources/key_dart.txt", warn = FA
 library(rvest)
 library(stringi)
 library(tidyverse)
+library(xml2)
 
 # set base date
 base_date <- Sys.Date()
@@ -27,23 +28,25 @@ ifelse(!dir.exists(file.path(main_dir, corps_code_zip_dir)),        dir.create(f
 ifelse(!dir.exists(file.path(main_dir, corps_code_unzip_dir)),      dir.create(file.path(main_dir, corps_code_unzip_dir)),      FALSE)
 ifelse(!dir.exists(file.path(main_dir, corps_code_parsed_csv_dir)), dir.create(file.path(main_dir, corps_code_parsed_csv_dir)), FALSE)
 
-# set addtional path and create directories related to a001
+# set additional path and create directories related to a001
 biz_report_list_xml_dir <- file.path("results/dart/biz_report_list/xml/")
 biz_report_list_csv_dir <- file.path("results/dart/biz_report_list/csv/")
 biz_report_zip          <- file.path("results/dart/biz_report/zip/")
+biz_report_xml          <- file.path("results/dart/biz_report/xml/")
 
 ifelse(!dir.exists(file.path(main_dir, biz_report_list_xml_dir)), dir.create(file.path(main_dir, biz_report_list_xml_dir)), FALSE)
 ifelse(!dir.exists(file.path(main_dir, biz_report_list_csv_dir)), dir.create(file.path(main_dir, biz_report_list_csv_dir)), FALSE)
 ifelse(!dir.exists(file.path(main_dir, biz_report_zip)),          dir.create(file.path(main_dir, biz_report_zip)),          FALSE)
+ifelse(!dir.exists(file.path(main_dir, biz_report_xml)),          dir.create(file.path(main_dir, biz_report_xml)),          FALSE)
 
-# set addtional path and create directories related to f001
+# set additional path and create directories related to f001
 audit_report_list_xml_dir <- file.path("results/dart/audit_report_list/xml/",     base_date, "/")
-audit_report_list_csv_dir <- file.path("results/dart/audit_report_list/csv/",     base_date, "/")
+audit_report_list_csv_dir <- file.path("results/dart/audit_report_list/csv/")
 audit_report_zip          <- file.path("results/dart/audit_report/zip/",          base_date, "/")
-audit_report_xml_from_biz <- file.path("results/dart/audit_report/xml/from_biz/", base_date, "/")
-audit_report_xml_from_aud <- file.path("results/dart/audit_report/xml/from_aud/", base_date, "/")
-audit_report_pdf_from_biz <- file.path("results/dart/audit_report/pdf/from_biz/", base_date, "/")
-audit_report_pdf_from_aud <- file.path("results/dart/audit_report/pdf/from_aud/", base_date, "/") 
+audit_report_xml_from_biz <- file.path("results/dart/audit_report/xml/from_biz/")
+audit_report_xml_from_aud <- file.path("results/dart/audit_report/xml/from_aud/")
+audit_report_pdf_from_biz <- file.path("results/dart/audit_report/pdf/from_biz/")
+audit_report_pdf_from_aud <- file.path("results/dart/audit_report/pdf/from_aud/") 
 
 ifelse(!dir.exists(file.path(main_dir, audit_report_list_xml_dir)), dir.create(file.path(main_dir, audit_report_list_xml_dir)), FALSE)
 ifelse(!dir.exists(file.path(main_dir, audit_report_list_csv_dir)), dir.create(file.path(main_dir, audit_report_list_csv_dir)), FALSE)
